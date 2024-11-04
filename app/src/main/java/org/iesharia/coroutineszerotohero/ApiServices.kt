d100126dd5f70de66999629661d41959e48a1bac
Emilio Haría <haria.informatica@gmail.com>
2024-11-04 13:06:10 +0000

    add suspend

--- app/src/main/java/org/iesharia/coroutineszerotohero/ApiServices.kt
+++ app/src/main/java/org/iesharia/coroutineszerotohero/ApiServices.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Path
 interface ApiServices {
 
     @GET("/api/14c14f8b4a12f4c1cd4098a5ea8092cb/search({name}")
-    fun getSuperheroes(@Path("name") superheroName:String): Response<SuperHeroDataResponse>
+    suspend fun getSuperheroes(@Path("name") superheroName:String): Response<SuperHeroDataResponse>
 }
 
 data class SuperHeroDataResponse(@SerializedName("response") val response:String)
